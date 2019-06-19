a0b37a3018925e47bd890fd18ae82004c36c8e93
Ellison Rio <rio.ellison@gmail.com>
2019-06-19 14:58:31 -0400

    created various component .js files

---
+++ nba-draft/src/CreatePlayer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreatePlayer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CreatePlayer;
---
+++ nba-draft/src/UpdatePlayer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UpdatePlayer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default UpdatePlayer;
