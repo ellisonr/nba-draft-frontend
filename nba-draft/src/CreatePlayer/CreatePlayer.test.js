d727afb65f6cd7bb1829672baa32e888d3dfc111
Ellison Rio <rio.ellison@gmail.com>
2019-06-20 14:32:44 -0400

    added jest and enzyme, no tests passing

---
+++ nba-draft/src/CreatePlayer/CreatePlayer.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import CreatePlayer from './CreatePlayer'
+
+// describe('CreatePlayer component', () => {
+//     it('should render as expected', () => {
+//         const component = shallow(<form></form>)
+//         expect(component.contains(component)).toEqual(true);
+//     });
+// })
--- nba-draft/src/DeletePlayer/DeletePlayer.js
+++ nba-draft/src/DeletePlayer/DeletePlayer.js
@@ -4,12 +4,15 @@ import axios from 'axios';
 
 class DeletePlayer extends Component {
     handleChange = e => {
-
+        e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
     handleDelete = e => {
         e.preventDefault();
-        axios.delete('http://localhost:8080/api/players', this.state)
+        axios.delete('http://localhost:8080/api/players/', this.state)
         .then(res => console.log(res))
         .catch(err => console.log(err))
     }
@@ -20,6 +23,9 @@ class DeletePlayer extends Component {
                 <form>
                     <div>
                         <h1 className="delete-player-header">Delete a Player</h1>
+                        <input type="text" name="name" onChange={this.handleChange} />
+
+                        <input type="submit" onClick={this.handleDelete} />
                     </div>
                 </form>
             </div>
