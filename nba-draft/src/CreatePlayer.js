fe684cae5c62960ef377ff51cb0eb6a20a9ea33b
Ellison Rio <rio.ellison@gmail.com>
2019-06-19 23:50:10 -0400

    createplayer kind of works, bare minimum css so far

--- nba-draft/src/CreatePlayer.js
+++ nba-draft/src/CreatePlayer.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import './CreatePlayer.css';
 import axios from 'axios';
 
 class CreatePlayer extends Component {
-    
+handleChange = e => {
+    e.preventDefault();
+    this.setState({
+        [e.target.name]: e.target.value
+    })
+}
+
+handleSubmit = e => {
+    e.preventDefault();
+    axios.post('http://localhost:8080/api/players', this.state)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
     render() {
         return (
             <div className="print-area">
                 <form>
                     <div>
-                        <h1>Add a Player</h1>
-                        <label>Name</label>
-                        <label>Age</label>
-                        <label>College</label>
+                        <h1 className="add-player-header">Add a Player</h1>
+                        
+                        <label>Name: </label>
+                        <input type="text" name="name" onChange={this.handleChange} />
+
+                        <label>Age: </label>
+                        <input type="text" name="age" onChange={this.handleChange} />
+
+                        <label>College: </label>
+                        <input type="text" name="college" onChange={this.handleChange} />
+
+                        <input type="submit" onClick={this.handleSubmit} />
                     </div>
                 </form>
             </div>
