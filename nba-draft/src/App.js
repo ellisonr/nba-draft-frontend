b39bac0a99dd251520f862d86dd2bfb138f4f6b8
Ellison Rio <rio.ellison@gmail.com>
2019-06-19 22:30:37 -0400

    installed axios, added css, started building home page with all players

--- nba-draft/src/App.js
+++ nba-draft/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import CreatePlayer from './CreatePlayer';
+import DeletePlayer from './DeletePlayer';
+import UpdatePlayer from './UpdatePlayer';
+import Nav from './Nav'
+import Players from './Players';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Switch>
+        <Route path='/' exact render={() => <Redirect to='/show' />}/>
+        <Route path='/show' component={Players} />
+        <Route path='/create' component={CreatePlayer} />
+        <Route path='/update' component={UpdatePlayer} />
+        <Route path='/delete' component={DeletePlayer} />
+      </Switch>
     </div>
   );
+  }
 }
 
 export default App;
--- nba-draft/src/Players.js
+++ nba-draft/src/Players.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import './Players.css'
 
 class Players extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            players: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8080/api/players')
+        .then(res => {
+            this.setState({players: res.data})
+        })
+    }
+
     render() {
+        let allPlayers = this.state.players.map((players, index) => {
+            return (
+                <div className="players" key={index}>
+                    <p className="playersDetails">Name: {players.name}</p>
+                    <p className="playersDetails2">Age: {players.age}</p>
+                    <p className="playersDetails2">College: {players.college}</p>
+                </div>
+            )
+        })
         return (
             <div>
-                
+                {allPlayers}
             </div>
         );
     }
