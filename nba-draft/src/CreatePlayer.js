0016cdbdff70abccc70fa55a6bb5ba4aeab97b3b
Ellison Rio <rio.ellison@gmail.com>
2019-06-19 23:20:23 -0400

    added more styling, added fonts to index.html, starting on createplayer.js

--- nba-draft/src/CreatePlayer.js
+++ nba-draft/src/CreatePlayer.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CreatePlayer extends Component {
+    
     render() {
         return (
-            <div>
-                
+            <div className="print-area">
+                <form>
+                    <div>
+                        <h1>Add a Player</h1>
+                        <label>Name</label>
+                        <label>Age</label>
+                        <label>College</label>
+                    </div>
+                </form>
             </div>
         );
     }
--- nba-draft/src/Nav.js
+++ nba-draft/src/Nav.js
@@ -5,10 +5,13 @@ import './Nav.css'
 class Nav extends Component {
     render() {
         return (
-            <div className="navbar">
+            <div className="nav-bar">
                 <h1 className="header">2019 NBA Prospects</h1>
                 <nav>
-                    
+                    <Link to='/show' className="nav-link">All Players</Link>
+                    <Link to='/create' className="nav-link">Add Player</Link>
+                    <Link to='/update' className="nav-link">Update Player Info</Link>
+                    <Link to='/delete' className="nav-link">Delete Player</Link>
                 </nav>
             </div>
         );
